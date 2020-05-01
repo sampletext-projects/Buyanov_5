168c6561c26af22e14c881ee01331f230bee1761
sampletext32 <sampletext32@bk.ru>
2020-05-01 17:08:29 +0300

    Add project

---
+++ Buyanov_5/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	cout << "Выполнил студент группы УТН-113 Рябов А.Д \n";
+	cout << "Программа для расчёта стоимости товара\n";
+	cout << "Введите стоимость 1 кг продукта: ";
+	double price;
+	cin >> price;
+	for (double grams = 50; grams < 1000; grams += 50)
+	{
+		double cost = grams / 1000 * price;
+		cout << grams << "г стоят " << cost << "р\n";
+	}
+
+	for (double grams = 1000; grams <= 10000; grams += 1000)
+	{
+		double cost = grams / 1000 * price;
+		cout << grams << "г стоят " << cost << "р\n";
+	}
+	system("pause");
+}
